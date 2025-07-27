cfe86bde192d404eeb0b6df74fb86c978677d78f
Patrick Borgogno <789401+borgiman@users.noreply.github.com>
2025-07-27 11:20:17 +0200

    feat: call binance api

--- Part2/main.cpp
+++ Part2/main.cpp
@@ -1,6 +1,31 @@
-﻿#include <iostream>
+﻿#include <curl/curl.h>
+#include <iostream>
+
+size_t WriteCallback(void* ptr, size_t size, size_t nmemb, std::string* userdata) {
+    userdata->append(static_cast<char*>(ptr), size * nmemb);
+    return size * nmemb;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    if (CURL* curl = curl_easy_init()) {
+        std::string response;
+        curl_easy_setopt(curl, CURLOPT_URL, "https://fapi.binance.com/fapi/v1/aggTrades?symbol=BTCUSDT&limit=5");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        CURLcode responseCode = curl_easy_perform(curl);
+
+        if (responseCode != CURLE_OK) {
+            std::cerr << "Request failed: " << curl_easy_strerror(responseCode) << std::endl;
+        }
+        else {
+            std::cout << response << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
     return 0;
 }
